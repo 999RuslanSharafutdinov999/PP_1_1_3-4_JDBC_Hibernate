c6b610e0a8ee38956dc65ca56fe159d7d924fb4f
999RuslanSharafutdinov999 <777tolst77794@mail.ru>
2025-09-10 23:39:12 +0500

    1.1.3-v1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,37 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь тут было изменение 23
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        System.out.println("Таблица создана");
+
+        userService.saveUser("Petr", "Sidorov",(byte) 42);
+        System.out.println("User с именем Petr добавлен в таблицу");
+        userService.saveUser("Gor", "Sirov",(byte) 34);
+        System.out.println("User с именем Gor добавлен в таблицу");
+        userService.saveUser("Any", "Sidorova",(byte) 31);
+        System.out.println("User с именем Any добавлен в таблицу");
+        userService.saveUser("Rus", "Orov",(byte) 20);
+        System.out.println("User с именем Rus добавлен в таблицу");
+
+        List <User> users = userService.getAllUsers();
+        System.out.println("Все пользователи из таблицы");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        System.out.println("Таблица очищена");
+
+        userService.dropUsersTable();
+        System.out.println("Таблица удалена");
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String URL = "jdbc:mysql://localhost:3306/usersBase";
+    public static final String USER = "Ruslan";
+    public static final String PASSWORD = "Amplified8908.";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected OK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Error");
+        }
+        return conn;
+    }
 }
+
+
