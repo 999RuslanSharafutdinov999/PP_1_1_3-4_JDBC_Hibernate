8c1831e667d02fec2c023d0ad1b59a465e741b06
999RuslanSharafutdinov999 <777tolst77794@mail.ru>
2025-09-20 19:14:45 +0500

    1.1.4-v1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,26 +1,63 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties properties = new Properties();
+                properties.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/usersBase");
+                properties.put("hibernate.connection.username", "Ruslan");
+                properties.put("hibernate.connection.password", "Amplified8908.");
+
+                properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+                properties.put("hibernate.show_sql", "true");
+                properties.put("hibernate.current_session_context_class", "thread");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+                        applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("ошибка Hibernate", e);
+            }
+        }
+        return sessionFactory;
+    }
+
     public static final String URL = "jdbc:mysql://localhost:3306/usersBase";
     public static final String USER = "Ruslan";
     public static final String PASSWORD = "Amplified8908.";
 
     public static Connection getConnection() {
-        Connection conn = null;
         try {
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected OK");
+            return conn;
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Connection Error");
+            return null;
         }
-        return conn;
     }
 }
-
-
